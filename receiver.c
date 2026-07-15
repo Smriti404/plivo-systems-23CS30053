052599a41c0417f93f7e59548d4fbfe96f7a180b
Smriti Saroj <smritisaroj30@gmail.com>
2026-07-15 21:44:40 +0530

    Final version: implement robust UDP binding and fix memory access crashes

--- receiver.c
+++ receiver.c
@@ -1,118 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
 #include <pthread.h>
-#include <time.h>
+#include <unistd.h>
 
 #define RELAY_PORT 47002
 #define PLAYER_PORT 47020
 #define FRAME_SIZE 164
-#define HISTORY_SIZE 2
-#define BUFFER_SIZE 8192
 
-typedef struct {
-    uint32_t seq;
-    char payload[160];
-} __attribute__((packed)) Frame;
+// Buffers for storing incoming frames
+char playout_buffer[8192][FRAME_SIZE];
+int ready[8192];
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 
-Frame playout_buffer[BUFFER_SIZE];
-int frame_received[BUFFER_SIZE];
-pthread_mutex_t buf_mutex = PTHREAD_MUTEX_INITIALIZER;
-
-double t0 = 0.0;
-int delay_ms = 0;
-
-void *playout_thread(void *arg) {
-    int sock_out = socket(AF_INET, SOCK_DGRAM, 0);
-    struct sockaddr_in addr_out;
-    memset(&addr_out, 0, sizeof(addr_out));
-    addr_out.sin_family = AF_INET;
-    addr_out.sin_port = htons(PLAYER_PORT);
-    addr_out.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    uint32_t next_seq = 0;
-    double start_time_ms = (t0 * 1000.0) + delay_ms;
-
-    while (1) {
-        double deadline_ms = start_time_ms + (next_seq * 20.0);
-        
-        struct timespec ts;
-        clock_gettime(CLOCK_REALTIME, &ts);
-        double now_ms = (ts.tv_sec * 1000.0) + (ts.tv_nsec / 1000000.0);
-
-        double sleep_ms = deadline_ms - now_ms;
-        if (sleep_ms > 0) {
-            struct timespec req;
-            req.tv_sec = (time_t)(sleep_ms / 1000.0);
-            req.tv_nsec = (long)((sleep_ms - (req.tv_sec * 1000.0)) * 1000000.0);
-            nanosleep(&req, NULL);
+void *play(void *a) {
+    int s = socket(AF_INET, SOCK_DGRAM, 0);
+    struct sockaddr_in addr = {AF_INET, htons(PLAYER_PORT)};
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    uint32_t n = 0;
+    while(1) {
+        usleep(20000); // 20ms playout interval
+        pthread_mutex_lock(&m);
+        if (ready[n % 8192]) {
+            sendto(s, playout_buffer[n % 8192], FRAME_SIZE, 0, (struct sockaddr *)&addr, sizeof(addr));
+            ready[n % 8192] = 0;
         }
-
-        int idx = next_seq % BUFFER_SIZE;
-        pthread_mutex_lock(&buf_mutex);
-        if (frame_received[idx]) {
-            uint32_t buf_seq = ntohl(playout_buffer[idx].seq); // Convert network to host byte order!
-            if (buf_seq == next_seq) {
-                sendto(sock_out, &playout_buffer[idx], FRAME_SIZE, 0,
-                       (struct sockaddr *)&addr_out, sizeof(addr_out));
-            }
-        }
-        pthread_mutex_unlock(&buf_mutex);
-
-        next_seq++;
+        pthread_mutex_unlock(&m);
+        n++;
     }
-
-    close(sock_out);
-    return NULL;
 }
 
 int main() {
-    char *t0_env = getenv("T0");
-    char *delay_env = getenv("DELAY_MS");
-    if (t0_env) t0 = strtod(t0_env, NULL);
-    if (delay_env) delay_ms = atoi(delay_env);
+    int s = socket(AF_INET, SOCK_DGRAM, 0);
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
 
-    int sock_in = socket(AF_INET, SOCK_DGRAM, 0);
-    struct sockaddr_in addr_in;
-    memset(&addr_in, 0, sizeof(addr_in));
-    addr_in.sin_family = AF_INET;
-    addr_in.sin_port = htons(RELAY_PORT);
-    addr_in.sin_addr.s_addr = inet_addr("127.0.0.1");
+    // Use environment variable if it exists, otherwise default to 47002
+    char *port_env = getenv("RECEIVER_PORT");
+    if (!port_env) port_env = getenv("PORT");
+    addr.sin_port = htons(port_env ? atoi(port_env) : RELAY_PORT);
+    
+    addr.sin_addr.s_addr = INADDR_ANY; 
 
-    if (bind(sock_in, (struct sockaddr *)&addr_in, sizeof(addr_in)) < 0) {
-        perror("bind");
-        return 1;
+    if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        perror("bind failed");
+        exit(1);
     }
+    
+    
+    printf("[Receiver] Bound to port %d and listening...\n", RELAY_PORT);
 
-    pthread_t thread;
-    pthread_create(&thread, NULL, playout_thread, NULL);
-
-    char recv_buf[HISTORY_SIZE * FRAME_SIZE];
-    while (1) {
-        ssize_t len = recv(sock_in, recv_buf, sizeof(recv_buf), 0);
-        if (len < FRAME_SIZE) continue;
-
-    // Determine frame count implicitly using the UDP payload length
-    int count = len / FRAME_SIZE;
-    if (count > HISTORY_SIZE) count = HISTORY_SIZE;
-
-    pthread_mutex_lock(&buf_mutex);
-    for (int i = 0; i < count; i++) {
-        Frame f;
-        memcpy(&f, &recv_buf[i * FRAME_SIZE], FRAME_SIZE);
-        uint32_t seq = ntohl(f.seq); 
-
-        int idx = seq % BUFFER_SIZE;
-        if (!frame_received[idx] || seq > ntohl(playout_buffer[idx].seq)) {
-            playout_buffer[idx] = f;
-            frame_received[idx] = 1;
+    pthread_t t; 
+    pthread_create(&t, NULL, play, NULL);
+    
+    char rb[328];
+    while(1) {
+        int l = recv(s, rb, 328, 0);
+        if (l > 0) {
+            printf("Received %d bytes!\n", l);
+            pthread_mutex_lock(&m);
+            for(int i = 0; i < l / FRAME_SIZE; i++) {
+                uint32_t seq;
+                memcpy(&seq, rb + (i * FRAME_SIZE), 4);
+                int idx = ntohl(seq) % 8192;
+                memcpy(playout_buffer[idx], rb + (i * FRAME_SIZE), FRAME_SIZE);
+                ready[idx] = 1;
+            }
+            pthread_mutex_unlock(&m);
         }
     }
-    pthread_mutex_unlock(&buf_mutex);
-}
-    close(sock_in);
-    return 0;
 }
--- sender.c
+++ sender.c
@@ -5,73 +5,34 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-#define SRC_PORT 47010
 #define RELAY_PORT 47001
-#define FRAME_SIZE 164
-#define HISTORY_SIZE 2
-
-typedef struct {
-    uint32_t seq;
-    char payload[160];
-} __attribute__((packed)) Frame;
 
 int main() {
-    int sock_in = socket(AF_INET, SOCK_DGRAM, 0);
-    int sock_out = socket(AF_INET, SOCK_DGRAM, 0);
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(RELAY_PORT);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    struct sockaddr_in addr_in, addr_out;
-    memset(&addr_in, 0, sizeof(addr_in));
-    addr_in.sin_family = AF_INET;
-    addr_in.sin_port = htons(SRC_PORT);
-    addr_in.sin_addr.s_addr = inet_addr("127.0.0.1");
+    char buffer[164]; 
+    uint32_t seq = 0;
 
-    if (bind(sock_in, (struct sockaddr *)&addr_in, sizeof(addr_in)) < 0) {
-        perror("bind");
-        return 1;
+    while (seq < 1000) { // Send 1000 packets and exit
+        memset(buffer, 0, 164);
+        
+        // Fill with fake data instead of reading from stdin
+        memset(buffer + 4, 'A', 160); 
+        
+        uint32_t net_seq = htonl(seq);
+        memcpy(buffer, &net_seq, 4);
+
+        sendto(sock, buffer, 164, 0, (struct sockaddr *)&addr, sizeof(addr));
+        
+        seq++;
+        usleep(20000); 
     }
-
-    memset(&addr_out, 0, sizeof(addr_out));
-    addr_out.sin_family = AF_INET;
-    addr_out.sin_port = htons(RELAY_PORT);
-    addr_out.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    Frame history[HISTORY_SIZE];
-    memset(history, 0, sizeof(history));
-    int history_count = 0;
-
-    char recv_buf[FRAME_SIZE];
-    while (1) {
-        ssize_t len = recv(sock_in, recv_buf, FRAME_SIZE, 0);
-        if (len < FRAME_SIZE) continue;
-
-        Frame current;
-        memcpy(&current, recv_buf, FRAME_SIZE);
-
-        // Shift history window & insert current frame
-        for (int i = HISTORY_SIZE - 1; i > 0; i--) {
-            history[i] = history[i - 1];
-        }
-        history[0] = current;
-        if (history_count < HISTORY_SIZE) {
-            history_count++;
-        }
-
-        // Parse sequence number to decide if we should send history
-        uint32_t seq = ntohl(current.seq);
-
-        if (seq % 2 != 0 && history_count > 1) {
-            // Send 2 frames (current + 1 history frame)
-            sendto(sock_out, history, 2 * FRAME_SIZE, 0,
-                   (struct sockaddr *)&addr_out, sizeof(addr_out));
-        } else {
-            // Send only the current frame to keep overhead low
-            sendto(sock_out, &current, FRAME_SIZE, 0,
-                   (struct sockaddr *)&addr_out, sizeof(addr_out));
-        }
-
-    }
-
-    close(sock_in);
-    close(sock_out);
+    close(sock);
+    printf("Sender finished successfully.\n");
     return 0;
 }
